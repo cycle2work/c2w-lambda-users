c02365d80033915c0b6be5e445e6627c6cefddf4
Manuel Serra <nitroin@hotmail.com>
2017-10-31 11:09:26 +0100

    [chore]: use local mongo while testing

--- src/config.js
+++ src/config.js
@@ -9,4 +9,4 @@ export const STRAVA_CLIENT_SECRET = process.env.STRAVA_CLIENT_SECRET;
 export const STRAVA_REDIRECT_URI = process.env.STRAVA_REDIRECT_URI;
 export const STRAVA_CODE_TEST = process.env.STRAVA_CODE_TEST;
 export const USERS_COLLECTION = process.env.USERS_COLLECTION || "users";
-export const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/test";
+export const MONGODB_URL = process.env.NODE_ENV !== "test" ? process.env.MONGODB_URL : "mongodb://localhost:27017/test";
