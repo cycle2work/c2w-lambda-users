ab9e704650f142274af888e35ef6e12c1c28ab50
Manuel Serra <nitroin@hotmail.com>
2020-07-21 18:06:12 +0200

    fix: invoke `getToken` instead of `refreshToken`

--- src/services/strava.js
+++ src/services/strava.js
@@ -1,4 +1,4 @@
 import strava from "strava-v3";
 
-export const getToken = (...args) => strava.oauth.refreshToken(...args);
+export const getToken = (...args) => strava.oauth.getToken(...args);
 export const listAthleteClubs = (...args) => strava.athlete.listClubs(...args);
